9eca87d9b712338399016e6f21a641e9ff54d9b8
franciscoMigliardi24 <francisco.migliardi@comunidad.ub.edu.ar>
2020-04-18 23:56:44 -0300

    working

--- clima.py
+++ clima.py
@@ -1,17 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://articulo.mercadolibre.com.ar/MLA-816680025-monitor-gamer-ultrawide-ips-25-pulg-lg-25um58-p-fullhd-cuota-_JM?quantity=1&variation=45349051022'
+URL = 'https://weather.com/es-AR/tiempo/hoy/l/ARBA0009:1:AR'
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
 
 page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(page.content, 'html.parser')   
 
-price = soup.find(id='productInfo').get_text()
-
-price = price.split('$')[1][0:8]
-price = float(price)
-if price > 20.000:
-    print(price)
+clima = soup.find(id='dp0-details-narrative').get_text()
+print(clima)
