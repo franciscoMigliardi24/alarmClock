a9aa9d672da599fb072b746e3b7ec9a7f74f91f2
franciscoMigliardi24 <francisco.migliardi@comunidad.ub.edu.ar>
2020-04-19 17:54:38 -0300

    main alarma

--- alarm.py
+++ alarm.py
@@ -1,8 +1,13 @@
 from gtts import gTTS
 from bs4 import BeautifulSoup
+from datetime import date
 import sys,subprocess, os, stat, requests, time
 
-URL = 'https://weather.com/es-AR/tiempo/hoy/l/ARBA0009:1:AR'
+
+#URL = 'https://weather.com/es-AR/tiempo/hoy/l/ARBA0009:1:AR'
+URL = 'https://www.meteored.com.ar/hoy/tiempo-en_Buenos+Aires-America+Sur-Argentina-Ciudad+Autonoma+de+Buenos+Aires-SABE-1-13584.html'
+
+
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
 
 page = requests.get(URL, headers=headers)
@@ -18,5 +23,13 @@ def speak(temp):
     time.sleep(5)
     os.remove("C:/Users/panch/Documents/alarmClock/temp.mp3")
 
-clima = soup.find(id='dp0-details-narrative').get_text()
-speak(clima)
+#clima = soup.find(id='dp0-details-narrative').get_text()
+clima = soup.find(class_='resumen-dia').get_text()
+
+#speak(clima)
+today = date.today()
+d1 = today.strftime("%d/%m/%Y") 
+print(d1)
+print(clima)
+texto = d1 + ' ' + clima
+speak(texto)
