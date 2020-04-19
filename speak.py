2231b74d3d1d9e79a866ba170b23cf5e63b3cb5b
franciscoMigliardi24 <francisco.migliardi@comunidad.ub.edu.ar>
2020-04-18 23:52:31 -0300

    working

--- speak.py
+++ speak.py
@@ -1,2 +1,18 @@
-import gTTS
-tts = gTTS('Hola mundo. Estamos convirtiendo texto a voz con Python.', lang='es-us')
+from gtts import gTTS
+from tempfile import TemporaryFile
+import time
+import sys,subprocess
+import os, stat
+#tts = gTTS('hello')
+
+#tts.save('hello.mp3')
+
+def speak(temp):
+    voice = gTTS(text=temp, lang="es-us")
+    voice.save("./temp.mp3")
+    os.startfile("C:/Users/panch/Documents/alarmClock/temp.mp3")
+    print("Speaking.....")
+    time.sleep(5)
+    os.remove("C:/Users/panch/Documents/alarmClock/temp.mp3")
+
+speak('forro como estas')
